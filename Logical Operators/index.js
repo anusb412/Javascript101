a617ed15a8fb4e6bd82298508297d4de803e185f
anusb412 <anusb412@gmail.com>
2024-11-30 12:48:53 -0700

    Logical Operators

---
+++ Logical Operators/index.js
@@ -0,0 +1,33 @@
+// logical operators = used to combine or manipulate boolean values (true / false)
+
+//                 AND = &&     BOTH NEEDS TO BE TRUE
+//                 OR = !!      1 NEEDS TO BE TRUE
+//                 NOT = !      changes true to false and false to true
+
+/* const temp = 20;
+
+if(temp > 0 && temp <= 30){
+    console.log("The weather is good");
+}
+else {
+    console.log("The weather is bad")
+}*/
+
+/*
+const temp = -250;
+
+if(temp <= 0 || temp > 30){
+    console.log("the weather is bad")
+}
+else {
+    console.log("The weather is Good")
+}*/
+
+const isSunny = true;
+
+if(!isSunny) {
+    console.log("It is Sunny");
+}
+else {
+    console.log("it is cloudy");
+}
