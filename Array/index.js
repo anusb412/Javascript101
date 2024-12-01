75e742d3d9338a448aaf77f396e6914aa855bb84
anusb412 <anusb412@gmail.com>
2024-12-01 10:07:53 -0700

    Morning Coding :)

--- Array/index.js
+++ Array/index.js
@@ -1,2 +1,19 @@
 // Array - a variable like structure that can hold more than 1 value
 
+let fruits = ["apple", "orange"];
+//fruits[1] = "coconut";
+
+
+
+//cycles through the array. PRINTS APPLE, ORANGE
+for(let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
+
+for(let fruit of fruits) {
+    console.log(fruit)
+}
+
+
+.sort() // sorts in alphabet order
+.reverse() // reverse alphabet order after sort
