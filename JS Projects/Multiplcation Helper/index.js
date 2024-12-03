93e40f0d18d2587800a3ed5247e4e2cde38bc968
anusb412 <anusb412@gmail.com>
2024-12-02 17:41:45 -0700

    1

--- JS Projects/Multiplcation Helper/index.js
+++ JS Projects/Multiplcation Helper/index.js
@@ -2,16 +2,9 @@ const score = document.getElementById("score");
 const input = parseFloat(document.getElementById("input").value);
 const submit = document.getElementById("submit");
 
-function randomNums() {
-    const x = Math.floor(Math.random() * 10) + 1;
-    const y = Math.floor(Math.random() * 10) + 1;
-}
+const x =  Math.floor(Math.random() * 10) + 1;
+const y = Math.floor(Math.random() * 10) + 1;
 
-function MultipleNums() {
-    
-}
-
-
-
+let question = `What is ${x} Multiplied by ${y}?`;
 
 
